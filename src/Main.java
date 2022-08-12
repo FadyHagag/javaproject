cc4b90424547cd97debcda8fc87799aac238cdab
Eng.FOda_H@g@g <fadyhagag32@gmail.com>
2022-08-12 17:26:38 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        int myVar;
+//        myVar=50;
+//        System.out.println( myVar );
+//        int val1=21;
+//        int val2=6;
+//        int val3=3;
+//        int val4=1;
+//        int reusult1= val1-val2/val3;
+//        int reusult2= (val1-val2)/val3;
+//        System.out.println(reusult1);
+//        System.out.println(reusult2);
+//
+//         do {
+//             System.out.println(val2);
+//             val2++;
+//
+//         }while (val2<10);
+//
+//         float[] thval=new float[3];
+//         thval[0]=2;
+//         thval[1]=4;
+//         thval[2]=6;
+//         float sum=0.0f;
+//         for(float vaue:thval){
+//             sum+=vaue;
+//             System.out.println(sum);
+//         }
+//
+//         for (int i=0;i<=10;i++){
+//
+//         }
+
+
+     swap(34,90);
+
+
+
+
+
+
+
+    }
+
+
+    static void swap(int i,int j){
+        int k=i;
+        i=j;
+        j=k;
+        if (i>0)
+            return;
+        System.out.println(i+" "+j);
+        return;
+    }
+    int x=44;
+    String ss=String.valueOf(x);
+    int i=3,j=43;
+    int re=i*j;
+
+
+
+//    static double[] prouducts(double amt,double rate,int years){
+//        double[] accunt= new double[years];
+//        for (int index=0;index<years;index++){
+//            int year = index+1;
+//            accunt[index]=
+//        }
+
+    }
+
+}
